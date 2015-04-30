691370ec2dc839de22f8899043167debc203cadc
jdoutreloux <justine.doutreloux@hotmail.be>
2015-04-30 14:19:51 +0200

    Dossiers supprimés

--- BarTender2/app/src/main/java/com/example/maximehulet/bartender/activities/OrderActivity.java
+++
@@ -1,7 +0,0 @@
-package com.example.maximehulet.bartender.activities;
-
-/**
- * Created by maximehulet on 27/04/15.
- */
-public class OrderActivity {
-}
--- Bartender1/app/src/main/java/be/uclouvain/lsinf1225/bartender/activity/OrderActivity.java
+++
@@ -1,7 +0,0 @@
-package be.uclouvain.lsinf1225.bartender.activity;
-
-/**
- * Created by maximehulet on 24/04/15.
- */
-public class OrderActivity {
-}
